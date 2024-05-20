c8d6cece449f51d2e764b0cdc9ce683f4427f378
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-20 20:17:53 +0530

    add verify user middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import { Customer } from "../models/customer.model";
+import ApiError from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+const verifyUser = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+
+    const customer = await Customer.findById(decodedToken?._id).select(
+      "-password -token"
+    );
+
+    if (!customer) {
+      throw new ApiError(401, "Invalid Token");
+    }
+
+    req.customer = customer;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid token");
+  }
+});
+
+export default verifyUser;
