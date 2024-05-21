e4c275c57bb293023c9f24aa76078cd0561bc8ac
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-21 12:51:20 +0530

    add timestamps to the schema

--- server/src/models/customer_detail.model.js
+++ server/src/models/customer_detail.model.js
@@ -1,37 +1,42 @@
 import mongoose from "mongoose";
 
-const customerDetailSchema = new mongoose.Schema({
-  customer_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Customer",
+const customerDetailSchema = new mongoose.Schema(
+  {
+    customer_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phonenumber: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      trim: true,
+      lowercase: true,
+    },
   },
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  address: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  phonenumber: {
-    type: Number,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  message: {
-    type: String,
-    trim: true,
-    lowercase: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const CustomerDetail = mongoose.model(
   "CustomerDetail",
