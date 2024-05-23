b073d75065706c8a568071e4236edd44b098a166
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-23 09:02:45 +0530

    add edit and cancel reservation routes

--- server/src/routes/reservation.routes.js
+++ server/src/routes/reservation.routes.js
@@ -1,9 +1,19 @@
 import Router from "express";
 import verifyUser from "../middlewares/auth.middleware.js";
-import { addReservation } from "../controllers/reservation.controller.js";
+import {
+  addReservation,
+  cancelReservation,
+  modifyReservation,
+} from "../controllers/reservation.controller.js";
 
 const reservationRouter = Router();
 
 reservationRouter.route("/create-reservation").post(verifyUser, addReservation);
+reservationRouter
+  .route("/edit-reservation/:reservationId")
+  .patch(verifyUser, modifyReservation);
+reservationRouter
+  .route("/cancel-reservation/:reservationId")
+  .get(verifyUser, cancelReservation);
 
 export default reservationRouter;
