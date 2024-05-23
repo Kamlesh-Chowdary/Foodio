e8e33109290513adf6454041f46e1663087b0158
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-23 10:02:24 +0530

    add edit dish endpoint

--- server/src/controllers/menu.controller.js
+++ server/src/controllers/menu.controller.js
@@ -1,7 +1,7 @@
-import { Menu } from "../models/menu.model";
+import { Menu } from "../models/menu.model.js";
 import ApiResponse from "../utils/apiResponse.js";
 import ApiError from "../utils/apiError.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const addDish = asyncHandler(async (req, res) => {
   const { name, category, description, price, rating } = req.body;
@@ -33,4 +33,48 @@ const addDish = asyncHandler(async (req, res) => {
     );
 });
 
-export { addDish };
+const editDish = asyncHandler(async (req, res) => {
+  const { dishId } = req.params;
+  if (!dishId) {
+    throw new ApiError(404, "Dish Id is required");
+  }
+  const { name, category, description, price, rating } = req.body;
+
+  if (isNaN(price)) throw new ApiError(409, "Price is required");
+
+  if (
+    [name, category, description, rating].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(409, "All the fields are required");
+  }
+
+  const dish = await Menu.findById(dishId).select("-__v");
+
+  if (!dish) {
+    throw new ApiError(404, "Invalid Dish Id");
+  }
+
+  const updateDish = await Menu.findByIdAndUpdate(
+    { _id: dishId },
+    {
+      name,
+      category,
+      description,
+      price,
+      rating,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updateDish) {
+    throw new ApiError(404, "Error while updating the dish details");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, updateDish, "Successfully Modified"));
+});
+
+export { addDish, editDish };
--- server/src/routes/menu.routes.js
+++ server/src/routes/menu.routes.js
@@ -1,9 +1,9 @@
 import Router from "express";
-import verifyUser from "../middlewares/auth.middleware";
-import { addDish } from "../controllers/menu.controller";
+import verifyUser from "../middlewares/auth.middleware.js";
+import { addDish, editDish } from "../controllers/menu.controller.js";
 
 const menuRouter = Router();
 
 menuRouter.route("/add-dish").post(verifyUser, addDish);
-
+menuRouter.route("/edit-dish/:dishId").post(verifyUser, editDish);
 export default menuRouter;
