db55b374ac7a24e3f63d6b8e65f28335a4c9f291
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-28 14:31:11 +0530

    add edit dish service for admin

--- client/src/services/menu.service.js
+++ client/src/services/menu.service.js
@@ -28,6 +28,35 @@ export class MenuService {
       throw error.response.data.message;
     }
   };
+
+  editDish = async (dishId, { name, category, description, price, rating }) => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("Token")}`,
+        },
+      };
+      const response = await axiosInstance.patch(
+        `/menu/edit-dish/${dishId}`,
+        {
+          name,
+          category,
+          description,
+          price,
+          rating,
+        },
+        config
+      );
+
+      return response.data;
+    } catch (error) {
+      console.log(
+        "Error while editing the dish :: editDish",
+        error.response.data.message
+      );
+      throw error.response.data.message;
+    }
+  };
 }
 
 const menuService = new MenuService();
