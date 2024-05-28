d06e26a25c1811d20fee44c88870264d2c3c7f19
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-28 15:10:47 +0530

    add create order service

---
+++ client/src/services/order.service.js
@@ -0,0 +1,34 @@
+import axiosInstance from "../helper/axiosInstance";
+
+export class OrderService {
+  createOrder = async (customerId, { items, message, paymentMethod }) => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("Token")}`,
+        },
+      };
+      const response = await axiosInstance.post(
+        "/order/create-order",
+        {
+          items,
+          message,
+          paymentMethod,
+          customerId,
+        },
+        config
+      );
+
+      return response.data;
+    } catch (error) {
+      console.log(
+        "Error while creating the order :: createOrder",
+        error.response.data.message
+      );
+      throw error.response.data.message;
+    }
+  };
+}
+
+const orderService = new OrderService();
+export default orderService;
