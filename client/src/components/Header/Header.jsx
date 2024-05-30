700f33197d74141b4c4916d56430add0d1243de9
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-30 20:32:45 +0530

    edit header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,14 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Logo from "../Logo";
 import { NavLink } from "react-router-dom";
+import Container from "../Container/Container";
+import { useState } from "react";
+import { X, Menu, ShoppingCart } from "lucide-react";
+
 const index = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+  const [display, setDisplay] = useState("hidden");
   const navItems = [
     {
       name: "Home",
@@ -27,25 +35,59 @@ const index = () => {
       path: "contact",
     },
   ];
+
+  const toggleNavbar = () => {
+    setIsNavbarVisible(!isNavbarVisible);
+    console.log(isNavbarVisible);
+    if (!isNavbarVisible) setDisplay("block");
+    else setDisplay("hidden");
+  };
   return (
-    <header>
-      <nav>
-        <div>
-          <Logo />
-        </div>
-        <div>
-          <ul>
-            {navItems.map((item) => {
-              return (
-                <NavLink key={item.name} to={item.path}>
-                  {item.name}
-                </NavLink>
-              );
-            })}
-          </ul>
-        </div>
-        <div></div>
-      </nav>
+    <header className="pt-5 md:pt-0">
+      <Container>
+        <nav className="md:flex md:justify-evenly md:w-full   items-center  md:mx-auto md:my-7">
+          <div className="flex justify-between  items-center mb-4 md:mb-0 ">
+            <Logo />
+            <div className="md:hidden">
+              <button onClick={toggleNavbar}>
+                {isNavbarVisible ? <X /> : <Menu />}
+              </button>
+            </div>
+          </div>
+
+          <div className={`${display} md:flex `}>
+            <ul className="flex flex-col gap-4 md:gap-8 md:flex-row">
+              {navItems.map((item) => {
+                return (
+                  <NavLink
+                    key={item.name}
+                    to={item.path}
+                    className={({ isActive }) =>
+                      `${
+                        isActive
+                          ? "text-[#F54748] border-b-2  border-[#F54748]"
+                          : "text-black "
+                      }`
+                    }
+                  >
+                    {item.name}
+                  </NavLink>
+                );
+              })}
+            </ul>
+          </div>
+          <div
+            className={`${display} md:flex  mt-4 flex md:mt-0  gap-4 justify-center items-center`}
+          >
+            <button className="rounded-full p-3 bg-white">
+              <ShoppingCart />
+            </button>
+            <button className="bg-[#F54748] text-white font-semibold rounded-3xl py-3 px-7">
+              Login
+            </button>
+          </div>
+        </nav>
+      </Container>
     </header>
   );
 };
