f5dd18331c27a804e1ee5d5efeaaa43e3638b497
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-05-31 20:34:37 +0530

    add index file for all exports

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import Container from "./Container/Container";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Logo from "./Logo";
+
+export { Container, Footer, Header, Logo };
