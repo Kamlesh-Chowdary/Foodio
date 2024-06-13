cc7e978fdcdd1bc40cd79726c07be07d248db438
Kamlesh <115933182+Kamlesh-Chowdary@users.noreply.github.com>
2024-06-13 17:34:57 +0530

    add menu component

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Container, Button, ItemCard } from "./index";
+import menuService from "../services/menu.service";
+const Menu = () => {
+  const [menuItems, setMenuItems] = useState([]);
+  console.log(menuItems);
+  useEffect(() => {
+    const fetchMenuItems = async () => {
+      try {
+        const response = await menuService.getMenu();
+        setMenuItems(response.data);
+      } catch (error) {
+        console.log("Error While fetching menu items", error);
+      }
+    };
+    fetchMenuItems();
+  }, []);
+  return (
+    <Container>
+      <section className="lg:w-5/6 py-5 m-auto ">
+        <h1 className="text-7xl font-bold text-center text-[#301E08] ">
+          Our Popular Menu
+        </h1>
+        <div className="grid grid-cols-3">
+          {menuItems.map((item) => (
+            <ItemCard item={item} />
+          ))}
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Menu;
