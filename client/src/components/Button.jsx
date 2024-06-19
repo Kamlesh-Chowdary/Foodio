aa728386d67faff09b377053f87dfe6f235b13e0
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-06-19 13:30:45 +0530

    fix: handle navigation to order or reservation page

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-
+import { useNavigate } from "react-router-dom";
 const Button = ({
   children,
   type = "button",
@@ -8,11 +8,18 @@ const Button = ({
   className = "",
   ...props
 }) => {
+  const navigate = useNavigate();
+  const handleNavigate = (e) => {
+    if (e.target.innerHTML === "Order now") navigate("/order");
+    else if (e.target.innerHTML === "Reservation") navigate("/reservation");
+  };
+
   return (
     <button
       type={type}
       {...props}
       className={`${bgColor} ${textColor} ${className} hover:-translate-y-1 hover:scale-90 text-nowrap`}
+      onClick={handleNavigate}
     >
       {children}
     </button>
