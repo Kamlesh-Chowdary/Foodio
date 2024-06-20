4e3c7b78b4823eaca611d7b8958326266a517049
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-06-20 20:43:11 +0530

    feat: Api call for inital fetching of menu items in menuSlice

--- client/src/store/menuSlice.js
+++ client/src/store/menuSlice.js
@@ -1,19 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import menuService from "../services/menu.service";
 
 const initialState = {
   menuItems: [],
+  status: "idle",
+  error: null,
 };
+
+export const fetchMenuItems = createAsyncThunk(
+  "menu/fetchMenuItems",
+  async (selectedCategory) => {
+    try {
+      const response = await menuService.getMenu(selectedCategory);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+);
 
 const menuSlice = createSlice({
   name: "menu",
   initialState,
-  reducers: {
-    setMenuItems: (state, action) => {
-      state.menuItems = action.payload;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMenuItems.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMenuItems.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.menuItems = action.payload;
+      })
+      .addCase(fetchMenuItems.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
   },
 });
-
-export const { setMenuItems } = menuSlice.actions;
 
 export default menuSlice.reducer;
