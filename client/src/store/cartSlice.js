8293d1010adda13cad9e554c75f59126fc44df62
kamlesh-chowdary <kamleshchoyal143@gmail.com>
2024-06-21 19:14:39 +0530

    feat: removeFromCart to decrement quantity

--- client/src/store/cartSlice.js
+++ client/src/store/cartSlice.js
@@ -18,8 +18,20 @@ const cartSlice = createSlice({
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
     },
+    removeFromCart:( state, action )=>{
+        const itemIndex = state.cartItems.findIndex(item=> item._id === action.payload._id);
+        if(itemIndex>=0){
+            if(state.cartItems[itemIndex].quantity > 1){
+                state.cartItems[itemIndex].quantity -= 1;
+            }
+            else{
+                state.cartItems.splice(itemIndex, 1);
+            }
+        }
+    }
+
   },
 });
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
